f5871c37fe1522a51651b09cb3f9e46555fb704d
Vladimir Nguyen <phu.nguyen@sotatek.com>
2022-12-27 10:37:30 +0700

    add ads 02

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Script from 'next/script'
 import GoogleAd from '@components/GoogleAd';
 
 export default function Home() {
@@ -9,9 +10,9 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3661812461656391"
+          crossorigin="anonymous" />
       </Head>
-
-      
       <GoogleAd slot="8502170963" googleAdId="ca-pub-3661812461656391"/>
       <Footer />
     </div>
