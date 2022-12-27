87b10bd40174bf8b034c4f1a0a866b8884c2541f
Vladimir Nguyen <phu.nguyen@sotatek.com>
2022-12-27 10:26:44 +0700

    add ads 01

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+      
       <GoogleAd slot="8502170963" googleAdId="ca-pub-3661812461656391"/>
       <Footer />
     </div>
